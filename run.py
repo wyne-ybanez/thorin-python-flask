9fc18cdc709c89470186bddde8cf357bac15ee9d
wyne-ybanez <wyne_01@icloud.com>
2021-03-09 14:04:54 +0000

    Added images to about section

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+# Imports
 import os
 from flask import Flask, render_template
 
-
+# Flask App
 app = Flask(__name__)
 
-
+# Decorators
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -24,7 +25,7 @@ def contact():
 def careers():
     return render_template("careers.html")
 
-
+# Run app on server
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
