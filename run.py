6c0bde1f1360c124946a22125d5fdcbf6be1085b
wyne-ybanez <wyne_01@icloud.com>
2021-03-12 16:38:47 +0000

    Added contact form, added URL links for names, added new template for URL links, Added Post method to contact form

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Imports
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Flask App
 app = Flask(__name__)
@@ -25,16 +25,27 @@ def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member = obj 
+    return render_template('member.html', member=member)
 
 
-@app.route("/contact")
+""" 
+Flask only accepts GET - need to create code for POST
+Request refers to contact form request 
+"""
+@app.route("/contact", methods=["GET","POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form['email'])
     return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html", page_title="Careers")
+    return render_template("careers.html", page_title="Come Work With Us!")
 
 
 # Run app on server
